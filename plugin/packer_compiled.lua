10139693e3a247927c42b2d9e8dfe6cd3110ac7b
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2023-04-29 22:33:46 +0200

    Add fzf to telescope

--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -154,6 +154,11 @@ _G.packer_plugins = {
     path = "/Users/kamilchmielewski/.local/share/nvim/site/pack/packer/start/plenary.nvim",
     url = "https://github.com/nvim-lua/plenary.nvim"
   },
+  ["telescope-fzf-native.nvim"] = {
+    loaded = true,
+    path = "/Users/kamilchmielewski/.local/share/nvim/site/pack/packer/start/telescope-fzf-native.nvim",
+    url = "https://github.com/nvim-telescope/telescope-fzf-native.nvim"
+  },
   ["telescope.nvim"] = {
     loaded = true,
     path = "/Users/kamilchmielewski/.local/share/nvim/site/pack/packer/start/telescope.nvim",

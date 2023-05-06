15d734c2f1c358839071ce1bbfba8e7956efab24
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2023-05-06 23:49:33 +0200

    Show only one diagnostic line

--- after/plugin/lsp-lines.lua
+++ after/plugin/lsp-lines.lua
@@ -4,3 +4,13 @@ if not status then
 end
 
 lsplines.setup()
+
+vim.api.nvim_create_autocmd("WinEnter", {
+    callback = function()
+        local floating = vim.api.nvim_win_get_config(0).relative ~= ""
+        vim.diagnostic.config({
+            virtual_text = floating,
+            virtual_lines = not floating,
+        })
+    end,
+})

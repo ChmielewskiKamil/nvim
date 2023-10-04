4769d946ec88cd1c461be6bb976eff184fd68575
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2023-10-04 21:42:08 +0200

    Add CSS highlighting

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,7 +1,7 @@
 require 'nvim-treesitter.configs'.setup {
     -- A list of parser names, or "all" (the five listed parsers should always be installed)
     ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "rust", "solidity", "javascript", "typescript", "html",
-        "json", "svelte", "yaml", "bash" },
+        "json", "svelte", "yaml", "bash", "css" },
 
     -- Install parsers synchronously (only applied to `ensure_installed`)
     sync_install = false,

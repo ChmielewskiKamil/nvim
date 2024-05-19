abc3fa4c5509f06809de7a902786b59ccc409bc6
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2024-05-19 16:43:48 +0200

    👷 Add packer bootstrapping

--- lua/kchmielewski/packer.lua
+++ lua/kchmielewski/packer.lua
@@ -1,3 +1,15 @@
+local ensure_packer = function()
+    local fn = vim.fn
+    local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+    if fn.empty(fn.glob(install_path)) > 0 then
+        fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
+        vim.cmd [[packadd packer.nvim]]
+        return true
+    end
+    return false
+end
+
+local packer_bootstrap = ensure_packer()
 -- This file can be loaded by calling `lua require('plugins')` from your init.vim
 
 -- Only required if you have packer configured as `opt`
@@ -144,4 +156,9 @@ return require('packer').startup(function(use)
             require("nvim-dap-virtual-text").setup()
         end
     }
+    -- Automatically set up your configuration after cloning packer.nvim
+    -- Put this at the end after all plugins
+    if packer_bootstrap then
+        require('packer').sync()
+    end
 end)
